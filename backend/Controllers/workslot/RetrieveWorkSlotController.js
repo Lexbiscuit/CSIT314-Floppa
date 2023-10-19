8ced360da2d209e5ec25bbed36f49e39f7d13a2e
YoursTru1y <102355877+YoursTru1y@users.noreply.github.com>
2023-10-19 22:02:27 +0800

    made minor changes
    see for urself

--- backend/Controllers/workslot/RetrieveWorkSlotController.js
+++ backend/Controllers/workslot/RetrieveWorkSlotController.js
@@ -1,1 +1,29 @@
 import { Prisma } from "@prisma/client";
+
+export default class RetrieveWorkSlotController {
+    constructor(prisma, req, res) {
+        this.prisma = prisma;
+        this.req = req;
+        this.res = res;
+    }
+
+    async retrieve() {
+        try {
+            const retrieveWorkSlot = await this.prisma.WorkSlot.findMany({
+                orderBy: [
+                    {
+                        WorkSlotId: "asc",
+                    },
+                ],
+            });
+
+            // 200 OK
+            this.res.status(200).json(retrieveWorkSlot);
+        } catch (err) {
+            console.error(err);
+
+            // 500 INTERNAL SERVER ERROR
+            this.res.status(500).send("Internal Server Error.");
+        }
+    }
+}
--- backend/Controllers/workslot/SearchWorkSlotController.js
+++ backend/Controllers/workslot/SearchWorkSlotController.js
@@ -1,1 +1,27 @@
 import { Prisma } from "@prisma/client";
+
+export default class SearchWorkSlotController {
+    constructor(prisma, req, res) {
+        this.prisma = prisma;
+        this.req = req;
+        this.res = res;
+    }
+
+    async search(WorkSlotId) {
+        try {
+            const searchWorkSlot = await this.prisma.WorkSlot.findUnique({
+                where: {
+                    WorkSlotId: Number(WorkSlotId),
+                },
+            });
+
+            // 200 OK
+            this.res.status(200).json(searchWorkSlot);
+        } catch (err) {
+            console.error(err);
+
+            // 500 INTERNAL SERVER ERROR
+            this.res.status(500).send("Internal Server Error.");
+        }
+    }
+}
