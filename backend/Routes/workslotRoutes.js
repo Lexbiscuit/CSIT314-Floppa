472bfdc1f291506b008a6a69b53db76a662d74d6
YoursTru1y <102355877+YoursTru1y@users.noreply.github.com>
2023-10-19 20:18:11 +0800

    created workslot and routes
    under the routes and controllers folder

---
+++ backend/Routes/workslotRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import CreateWorkSlotController from "../Controllers/Profile/CreateWorkSlotController.js";
+import DeleteWorkSlotController from "../Controllers/Profile/DeleteWorkSlotController.js";
+import RetreiveWorkSlotController from "../Controllers/Profile/RetreiveWorkSlotController.js";
+import SearchWorkSlotController from "../Controllers/Profile/SearchWorkSlotController.js";
+import UpdateWorkSLotController from "../Controllers/Profile/UpdateWorkSLotController.js";
+import auth from "../auth.cjs";
+
