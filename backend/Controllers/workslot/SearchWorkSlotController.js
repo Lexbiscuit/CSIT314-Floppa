908251c2db529d7e8d23b614c97a6183426612ef
Lexus L <36026227+Lexbiscuit@users.noreply.github.com>
2023-10-26 20:39:08 +0800

    Revert "Update SearchWorkSlotController.js"
    This reverts commit 6d25619c723979a0ee4abf475945fbe907276c82.

--- backend/Controllers/workslot/SearchWorkSlotController.js
+++ backend/Controllers/workslot/SearchWorkSlotController.js
@@ -7,28 +7,16 @@ export default class SearchWorkSlotController {
     this.res = res;
   }
 
-  async viewStaffWithAssignedWorkSlots() {
+  async searchWorkslot(workslotId) {
     try {
-      // Step 2: Retrieve all records of staff that have work slots assigned to them.
-      const staffWithAssignedWorkSlots = await this.prisma.Staff.findMany({
+      const searchWorkslot = await this.prisma.workslots.findUnique({
         where: {
-          // Modify the condition to filter staff with assigned work slots.
-          // You need to have a relation between staff and work slots in your database.
-          workslots: {
-            some: {
-              // Define the condition that indicates a work slot is assigned to the staff.
-              // For example, you might check if a staff ID is associated with a work slot.
-              // Adjust this condition to match your schema.
-              staffId: {
-                not: null,
-              },
-            },
-          },
+          workslotId: Number(workslotId),
         },
       });
 
-      // Step 3: Display all records of staff with assigned work slots.
-      this.res.status(200).json(staffWithAssignedWorkSlots);
+      // 200 OK
+      this.res.status(200).json(searchWorkslot);
     } catch (err) {
       console.error(err);
 
