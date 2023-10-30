b73ccadac60b1c49424dc8def1c0303a758bb664
gab1291 <147357478+gab1291@users.noreply.github.com>
2023-10-30 17:01:18 +0800

    Update MngrFltrAvailWSCtrl.js

---
+++ backend/Controllers/Bid/MngrFltrAvailWSCtrl.js
@@ -0,0 +1,41 @@
+import { Prisma } from "@prisma/client";
+
+export default class MngrFltrAvailWSCtrl {
+  constructor(prisma, req, res) {
+    this.prisma = prisma;
+    this.req = req;
+    this.res = res;
+  }
+
+  /**
+   * Retrieves available work slots for the Cafe Manager.
+   *
+   * @returns {Workslots[]} - An array of available work slots.
+   */
+  async rtrvAvailWS() {
+    try {
+      
+      const availableWorkslots = await this.prisma.Workslots.findMany({
+        
+        where: {
+           bids: null,
+        },
+        
+        orderBy: [
+          {
+            workslotId: "asc",
+          },
+        ],
+      });
+
+
+      this.res.status(200).json(availableWorkslots);
+    } catch (err) {
+      console.error(err);
+
+      // 500 INTERNAL SERVER ERROR
+      console.log(err);
+      this.res.status(500).send({ message: "Internal Server Error." });
+    }
+  }
+}
