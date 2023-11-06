9f7b4f5b584c1f3b5dd2c427fc6e5d8bcde14d65
YoursTru1y <102355877+YoursTru1y@users.noreply.github.com>
2023-11-06 16:07:50 +0800

    updated StaffBidRoutes.js

---
+++ backend/Routes/StaffbidRoutes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import StaffRtrvBidCtlr from "../Controllers/StaffBid/StffRtrvBidsCtlr";
+import StaffBidSlotCtlr from "../Controllers/StaffBid/StaffBidSlotCtlr";
+import StaffUpdBidCtlr from "../Controllers/StaffBid/StaffUpdBidCtlr";
+import StaffDltBidSlotCtlr from "../Controllers/StaffBid/StffDltBidSlotCtlr";
+import StaffSrchBidCtlr from "../Controllers/StaffBid/StaffSrchBidCtlr";
+import StaffRtrvAvailWrkslotCtlr from "../Controllers/StaffBid/StffRtrvAvailWrksltCtlr";
+import StaffRtrvBidsRslt from "../Controllers/StaffBid/StffRtrvBidsRslt";
+import CreateBidController from "../Controllers/StaffBid/StaffBidSlotCtlr";
+
+const StaffbidRoutes = Router();
+const prisma = new PrismaClient();
+
+StaffbidRoutes.post("/create", async (req, res) => {
+    const staffbid = req.body;
+
+    const CreateBidController = new CreateBidController(prisma, req, res);
+    await CreateBidController.createBid(staffbid);
+});
+
+StaffbidRoutes.get("/retrieve", async (req, res) => {
+    const staffbid = req.body
+})
+
+export default StaffbidRoutes;
