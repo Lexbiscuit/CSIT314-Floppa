3b533d158df80442ef1a8b320d9f8951969e50cb
Lexus L <36026227+Lexbiscuit@users.noreply.github.com>
2023-11-14 01:05:16 +0800

    Update: implement validation
    1. Chef, cashier and waiter count must be > 0.
    2. startTime cannot be later than endTime.

--- backend/Controllers/Workslot/CreateWorkSlotController.js
+++ backend/Controllers/Workslot/CreateWorkSlotController.js
@@ -11,6 +11,24 @@ export default class CreateWorkslotController {
 
   async createWorkslot(workslot) {
     try {
+      // Work slot validation
+      const { cashiers, chefs, waiters } = workslot;
+      const startTime = DateTime.fromISO(workslot.startTime);
+      const endTime = DateTime.fromISO(workslot.endTime);
+
+      if (startTime > endTime) {
+        return this.res.status(500).send({
+          message: "Start time must be before end time. Workslot not created.",
+        });
+      }
+
+      if (cashiers < 1 || chefs < 1 || waiters < 1) {
+        return this.res.status(500).send({
+          message:
+            "Number of cashiers, chefs and waiters must be positive and > 0. Workslot not created.",
+        });
+      }
+
       const workslots = new Workslots(this.prisma);
       workslot["weekNumber"] = DateTime.fromISO(workslot.startTime).weekNumber;
       const response = await workslots.createWorkslot(workslot);
--- backend/Controllers/Workslot/UpdateWorkSlotController.js
+++ backend/Controllers/Workslot/UpdateWorkSlotController.js
@@ -10,6 +10,24 @@ export default class UpdateWorkslotController {
 
   async updateWorkslot(workslot) {
     try {
+      // Work slot validation
+      const { cashiers, chefs, waiters } = workslot;
+      const startTime = DateTime.fromISO(workslot.startTime);
+      const endTime = DateTime.fromISO(workslot.endTime);
+
+      if (startTime > endTime) {
+        return this.res.status(500).send({
+          message: "Start time must be before end time. Workslot not updated.",
+        });
+      }
+
+      if (cashiers < 1 || chefs < 1 || waiters < 1) {
+        return this.res.status(500).send({
+          message:
+            "Number of cashiers, chefs and waiters must be positive and > 0. Workslot not updated.",
+        });
+      }
+
       const workslots = new Workslots(this.prisma);
       const response = await workslots.updateWorkslot(workslot);
 
